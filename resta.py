38b78629676e7b3babba0100734114f3eeecc379
Bastián Alexander <ba.espejo@duocuc.cl>
2025-06-26 14:19:56 -0400

    Added funcion resta

---
+++ resta.py
@@ -0,0 +1,11 @@
+def sumar(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+n1 = int(input("Ingrese valor: "))
+n2 = int(input("Ingrese valor: "))
+
+print(f"La suma es: {sumar(n1,n2)}")
+print(f"La resta es: {resta(n1,n2)}")
